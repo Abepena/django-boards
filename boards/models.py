07a78389e7cc2815246fada96c5f141c3d2a8c2d
abepena <pena.abe@gmail.com>
2018-07-31 18:23:04 -0400

    finished models for boards app

--- boards/models.py
+++ boards/models.py
@@ -1,11 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
- class Board:
-     pass
 
-class Topic:
-    pass
 
-class Post:
-    pass
+class Board(models.Model):
+    # unique = True enforces uniqueness at the database level
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    """
+    related_name is optional, Django will auto-generate the related name as
+    <class_name>_set if nothing is specified
+    ex. Topic --> related_name = topic_set by default unless otherwise specified
+    """
+    subject = models.CharField(max_length=255)
+
+    last_update = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(
+        Board, on_delete=models.CASCADE, related_name="topics")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="topics")
+
+
+class Post(models.Model):
+    """
+    The updated_by attribute has a related_name of '+', this just tells Django 
+    that we dont need this reverse relationship so it will ignore it
+
+    ex. created_by relationship is needed
+        updated_by relationship isnt necessary information for us to display
+    """
+    message = models.CharField(max_length=4000)
+    # auto_now_add ensures that when a post is created the created datetime is now
+    created_at = models.DateTimeField(auto_now=True)
+    # null = True initializes the post with no updated_at attr populated yet
+    updated_at = models.DateField(null=True)
+    topic = models.ForeignKey(
+        Topic, on_delete=models.CASCADE, related_name="posts")
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts")
+    updated_by = models.ForeignKey(
+        User, null=True, on_delete=models.CASCADE, related_name="+")
