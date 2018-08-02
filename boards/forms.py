62d9aa7044faa67398c26becbbe4a5645dcf785b
abepena <pena.abe@gmail.com>
2018-08-02 12:25:53 -0400

    working Django API form with bootstrap style, added requirements as well

--- boards/forms.py
+++ boards/forms.py
@@ -6,10 +6,20 @@ class NewTopicForm(forms.ModelForm):
     This will eventually replace the naive implementation of the
     new_topic form using the Django Forms API
     """
-    message = forms.CharField(widget=forms.Textarea, max_length=4000)
+    # message to be cleaned and later passed into a Post object
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "rows": 5,
+                "placeholder": "Message to start the topic off",
+            }
+        ),
+        max_length=4000,
+        help_text="The max length of this box is 4000",
+    )
 
     class Meta:
         model = Topic
         # The subject field refers to the subject field in the Topic 
-        # model and message will refer to the message in Post model
+        # model and message will refer to the message later cleaned and created in a Post object
         fields = ['subject', 'message']
