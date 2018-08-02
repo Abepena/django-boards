22f60a7863a5cbf5b0cf90b09149c60ce08ee776
abepena <pena.abe@gmail.com>
2018-08-02 17:18:19 -0400

    edited signup page views and wrote more tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,14 +1,24 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
+from django.contrib.auth.forms import UserCreationForm
 from .views import signup
 # Create your tests here.
 
 class SignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
     def test_signup_response_status_code(self):
-        url = reverse("signup")
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
     
     def test_signup_url_resolves_to_signup_view(self):
         view = resolve("/signup/")
         self.assertEqual(view.func, signup)
+    
+    def test_csrf(self):
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+    
+    def test_contains_form(self):
+        form = self.response.context.get("form")
+        self.assertIsInstance(form, UserCreationForm)
